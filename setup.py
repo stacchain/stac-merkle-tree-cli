89deed3adbc1ef31097eea1517e5bf41263d444c
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-16 14:57:02 +0800

    change license string

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
