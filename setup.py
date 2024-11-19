9711a801d37c16d1205080cf37f69f841dcf4945
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-20 01:08:38 +0800

    update setup for PEP 625

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stac-merkle-tree-cli',
+    name='stac_merkle_tree_cli',
     version='0.3.0',
     author='Jonathan Healy',
     author_email='jonathan.d.healy@gmail.com',
